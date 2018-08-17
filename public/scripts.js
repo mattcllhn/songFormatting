d497f90c0186e714f627848aecd9642d72585641
matt <mattcllhn@gmail.com>
2018-08-17 13:16:30 -0500

    server returning formatted songs

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,32 @@
 
 console.log('scripts still running');
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '/songs/');
-xhr.onload = function(){
-  var local = xhr.responseText;
-  console.log('in func', local);
-};
+let songList = [];
+function getSongs() {
+  console.log('hello form getsongs')
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/songs/');
+  xhr.send(null);
+  xhr.onreadystatechange = function () {
+    var DONE = 4; // readyState 4 means the request is done.
+    var OK = 200; // status 200 is a successful return.
+    if (xhr.readyState === DONE) {
+      if (xhr.status === OK) {
+        let res = JSON.parse(xhr.responseText);
+        for (let i = 0; i < res.length; i++) {
+          songList.push(res[i].data)
+
+        }
+        console.log('songList', songList);
+
+      } else {
+        console.log('Error: ' + xhr.status); // An error occurred during the request.
+      }
+    }
+  }
+
+  // xhr.onload = function () {
+  //   var local = xhr.responseText;
+  //   console.log('in func', local);
+  // };
+}
+
--- server/index.js
+++ server/index.js
@@ -2,64 +2,60 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs');
-xml2js = require('xml2js');
+const xml2js = require('xml2js');
 const parser = new xml2js.Parser();
-
 
 app.use(express.static('../public'))
 
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log('listening on 3000')
 });
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
   console.log('holy balls its a server');
   res.sendFile(path.resolve('index.html'))
 });
 
-app.get('/songs',function (req, res){
-  const rootPath = '../../../../volumes/untitled/songs';
+app.get('/songs', function (req, res) {
+  console.log('get called in /songs');
+  const rootPath = '../../Downloads/songs';
   const folders = getDirectories(rootPath);
-  const path = rootPath + '/' + folders[0] + '/data.xml';
-  folders.forEach()
-  fs.readFile(path, (err, data) => {
-    parser.parseString(data, function (err, result) {
-      // console.log('data', result);
-      console.dir(result.data)
-      res.send(JSON.stringify(result.data));
-      // oneSong = result;
-    })
-  });
-  // res.send(songArray);
-});
-
-async function returnSongs(){
-  const rootPath = '../../../../volumes/untitled/songs';
-  const folders = getDirectories(rootPath);
-  let returnSongs = [];
-  for (var i = 0; i < folders.length; i++) {
-    const path = rootPath + '/' + folders[i] + '/data.xml';
-    const song = await getSong(path);
-    returnSongs.push(song);
-  }
-  return returnSongs;
-}
-async function getSong(path){
-  let oneSong;
-    fs.readFile(path, (err, data) => {
-      parser.parseString(data, function (err, result) {
-        // console.log('data', result);
-        oneSong = result;
-      })
+  let songArr = [];
+  for (let i = 0; i < folders.length; i++) {
+    const loopPath = rootPath + '/' + folders[i] + '/data.xml';
+    let song = fs.readFileAsync(loopPath).then((song) => {
+      return parser.parseStringAsync(song);
     });
-    console.log(oneSong);
-    return oneSong;
-}
+    songArr.push(song);
+  }
+  Promise.all(songArr).then(function (values) {
+    res.send(JSON.stringify(values));
+  });
+});
+parser.parseStringAsync = function (song) {
+  return new Promise(function (resolve, reject) {
+    parser.parseString(song, function (err, result) {
+      if (err)
+        reject(err);
+      else
+        resolve(result);
+    });
+  });
+};
 
+fs.readFileAsync = function (filename) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filename, function (err, data) {
+      if (err)
+        reject(err);
+      else
+        resolve(data);
+    });
+  });
+};
 
 function getDirectories(path) {
   return fs.readdirSync(path).filter((file) => {
-    return fs.statSync(path+'/'+file).isDirectory();
+    return fs.statSync(path + '/' + file).isDirectory();
   });
 }
