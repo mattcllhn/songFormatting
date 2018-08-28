d594d13729f8b4d8baf5253c5e7395914d9409f7
matt <mattcllhn@gmail.com>
2018-08-28 14:01:29 -0500

    songs returning and appended to dom

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,52 @@
 
 console.log('scripts still running');
 let songList = [];
+
+function renderSongs(){
+  console.log('renderSongs')
+for (let i = 0; i < songList.length; i++) {
+  let songRow = createDomElement('div','col-xs-12');
+  let heading = createDomElement('h4', 'title', songList[i].Song[0].title);
+  heading.onclick = function () {
+    if (this.parentElement.children[1].classList[1]=='expanded'){
+      this.parentElement.children[1].classList.remove('expanded');
+
+    }
+    this.parentElement.children[1].classList.add('expanded');
+  };
+  songRow.appendChild(heading);
+
+  let songEc = createDomElement('div', 'expand-collapse', formatSong(songList[i].Arrangement[0].text));
+
+  songRow.appendChild(songEc);
+  
+  document.getElementById('song-target').appendChild(songRow);
+}
+  // console.log(document.g?etElementById('song-target'));
+
+}
+
+function createDomElement(tag, className, content){
+let element = document.createElement(tag);
+if(element && className){
+  element.classList.add(className)
+}
+if(element && content){
+  element.appendChild(document.createTextNode(content));
+}
+return element;
+
+}
+function formatSong(song){
+  let unformattedSong = song[0]
+  let formattedSong = unformattedSong
+    .replace(/\[ \]/g, "")
+    .replace(/\{\,\}/g, '\n');
+  console.log(formattedSong);
+
+  return formattedSong;
+
+}
 function getSongs() {
   console.log('hello form getsongs')
   var xhr = new XMLHttpRequest();
@@ -14,19 +60,13 @@ function getSongs() {
         let res = JSON.parse(xhr.responseText);
         for (let i = 0; i < res.length; i++) {
           songList.push(res[i].data)
-
         }
         console.log('songList', songList);
-
+        renderSongs();
       } else {
         console.log('Error: ' + xhr.status); // An error occurred during the request.
       }
     }
   }
-
-  // xhr.onload = function () {
-  //   var local = xhr.responseText;
-  //   console.log('in func', local);
-  // };
 }
 
